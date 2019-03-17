1e56fcf51abcb016d30851d7d7e49e0eaffd9e35
Egor Lavrenov <elavrenow@gmail.com>
2019-03-17 23:57:02 +0300

    added validation for unnecessary functional

--- BLL.CoreEntities/Entities/Category.cs
+++ BLL.CoreEntities/Entities/Category.cs
@@ -1,9 +1,17 @@
-﻿namespace BLL.CoreEntities.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BLL.CoreEntities.Entities
 {
     public class Category
     {
         public int CategoryId { get; set; }
+        [Required]
+        [MinLength(3)]
+        [StringLength(100)]
         public string CategoryName { get; set; }
+        [Required]
+        [MinLength(3)]
+        [StringLength(100)]
         public string Description { get; set; }
     }
 }
