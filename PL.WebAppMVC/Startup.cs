04a5791155452e2bfc166422fbfec57c322a2c36
Egor Lavrenov <elavrenow@gmail.com>
2019-03-24 22:41:49 +0300

    Return changes for viewing errors

--- PL.WebAppMVC/Startup.cs
+++ PL.WebAppMVC/Startup.cs
@@ -70,29 +70,29 @@ namespace PL.WebAppMVC
                 app.UseHsts();
             }
 
-            //app.UseExceptionHandler(errorApp =>
-            //{
-            //    errorApp.Run(async context =>
-            //    {
-            //        context.Response.StatusCode = 500;
-            //        context.Response.ContentType = "text/html";
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "text/html";
 
-            //        await context.Response.WriteAsync("<html lang=\"en\"><body>\r\n");
-            //        await context.Response.WriteAsync("ERROR!<br><br>\r\n");
+                    await context.Response.WriteAsync("<html lang=\"en\"><body>\r\n");
+                    await context.Response.WriteAsync("ERROR!<br><br>\r\n");
 
-            //        var exceptionHandlerPathFeature =
-            //            context.Features.Get<IExceptionHandlerPathFeature>();
+                    var exceptionHandlerPathFeature =
+                        context.Features.Get<IExceptionHandlerPathFeature>();
 
-            //        if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
-            //        {
-            //            await context.Response.WriteAsync("File error thrown!<br><br>\r\n");
-            //        }
+                    if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+                    {
+                        await context.Response.WriteAsync("File error thrown!<br><br>\r\n");
+                    }
 
-            //        await context.Response.WriteAsync("<a href=\"/\">Home</a><br>\r\n");
-            //        await context.Response.WriteAsync("</body></html>\r\n");
-            //        await context.Response.WriteAsync(new string(' ', 512)); // IE padding
-            //    });
-            //});
+                    await context.Response.WriteAsync("<a href=\"/\">Home</a><br>\r\n");
+                    await context.Response.WriteAsync("</body></html>\r\n");
+                    await context.Response.WriteAsync(new string(' ', 512)); // IE padding
+                });
+            });
 
             app.UseStatusCodePages("text/plain", "Status code page, status code: {0}");
             app.UseHttpsRedirection();
