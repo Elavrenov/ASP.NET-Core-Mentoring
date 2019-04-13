ea159ccc1e281e34c7af2538a069b6427e2b9c9c
Egor Lavrenov <elavrenow@gmail.com>
2019-04-14 01:37:21 +0300

    Task 3. Html Helpers / Html Tags (Tag Helpers)

---
+++ PL.WebAppMVC/App_Code/NorthwindImageHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using BLL.CoreEntities.Entities;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PL.WebAppMVC
+{
+    public static class NorthwindImageHelper
+    {
+        public static HtmlString NorthwindImageLink(this IHtmlHelper html, Category category, int widht, int height)
+        {
+            TagBuilder a = new TagBuilder("a");
+            a.Attributes.Add("href", $"Categories/Image/{category.CategoryId}");
+            TagBuilder img = new TagBuilder("img");
+            img.Attributes.Add("width", $"{widht}px");
+            img.Attributes.Add("height", $"{height}px");
+            img.Attributes.Add("src", $"data:image/jpeg;base64,{Convert.ToBase64String(category.Picture)}");
+            a.InnerHtml.AppendHtml(img);
+
+            var writer = new System.IO.StringWriter();
+            a.WriteTo(writer, HtmlEncoder.Default);
+            return new HtmlString(writer.ToString());
+        }
+    }
+}
